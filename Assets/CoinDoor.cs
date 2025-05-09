5cfff61b4e16a8af72076dcce4bcdc09a774f734
S CL <51391741+sclchuck@users.noreply.github.com>
2025-05-09 12:21:34 +0800

    coindoor

---
+++ Assets/CoinDoor.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CoinDoor : MonoBehaviour
+{
+    public void Open()
+    {
+        var tw = gameObject.GetOrAddComponent<Tween>();
+        tw.AddTween("down", (x) => transform.position = transform.position.NewY(x), 0, -50, 0.1f);
+        tw.Play();
+    }
+}
